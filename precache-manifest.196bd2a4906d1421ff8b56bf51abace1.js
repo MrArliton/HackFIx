65f1f476c346830a41d467a4e43a584c4ba47ff8
root <Mr.arliton@yandex.ru>
2020-09-09 22:25:19 +0500

    Updates

---
+++ precache-manifest.196bd2a4906d1421ff8b56bf51abace1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ae50eb108bae1270544f0d1fbc465f",
+    "url": "/HackWebFix/index.html"
+  },
+  {
+    "revision": "892b98cfc5d021df33c5",
+    "url": "/HackWebFix/static/css/main.b37a7733.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/HackWebFix/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/HackWebFix/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892b98cfc5d021df33c5",
+    "url": "/HackWebFix/static/js/main.5a95dc80.chunk.js"
+  },
+  {
+    "revision": "c5de8103b3110fca4200",
+    "url": "/HackWebFix/static/js/runtime-main.5e7230cb.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/HackWebFix/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/HackWebFix/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/HackWebFix/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/HackWebFix/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/HackWebFix/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/HackWebFix/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/HackWebFix/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/HackWebFix/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/HackWebFix/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/HackWebFix/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/HackWebFix/static/media/sub.4eb05fdd.png"
+  }
+]);
